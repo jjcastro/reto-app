50625343d64fef1fa9c232d117ef889f63844caa
J.J. Castro <jj.castro10@uniandes.edu.co>
2016-12-31 20:32:09 -0500

    Initial Commit

---
+++ Reto/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Reto
+//
+//  Created by Juan José Castro on 12/31/16.
+//  Copyright © 2016 Juan Castro V. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
